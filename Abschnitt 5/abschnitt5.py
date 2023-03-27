a9deee7bb32aeb8e019aefa2fee82b64baa794b1
Feyu <67538041+Feyu87@users.noreply.github.com>
2023-03-27 09:00:36 +0200

    Update abschnitt5.py
    imported matrix 8x8 components. Tested component works. Failure between line 69 and 72 (if else condition failure.)

--- Abschnitt 5/abschnitt5.py
+++ Abschnitt 5/abschnitt5.py
@@ -6,6 +6,14 @@
 import RPi.GPIO as GPIO
 import smbus
 import time
+import re
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
 if(GPIO.RPI_REVISION == 1):
     bus = smbus.SMBus(0)
 else:
@@ -40,16 +48,40 @@ class LightSensor():
     def readLight(self):
         data = bus.read_i2c_block_data(self.DEVICE,self.ONE_TIME_HIGH_RES_MODE_1)
         return self.convertToNumber(data)
-def main():
+def main(cascaded, block_orientation, rotate):
+    # Matrix Gerät festlegen und erstellen. 
+    serial = spi(port=0, device=1, gpio=noop()) 
+    device = max7219(serial, cascaded=cascaded or 1, block_orientation=block_orientation, 
+    rotate=rotate or 0) 
+    # Matrix Initialisierung in der Konsole anzeigen 
+    print("[-]")
+    
+    # Hallo Welt in der Matrix anzeigen 
+    msg = "+"
+    # Ausgegebenen Text in der Konsole Anzeigen 
+    print("[-] Printing: %s" % msg) 
+    show_message(device, msg, fill="white", font=proportional(CP437_FONT), scroll_delay=0.1)
+    
     sensor = LightSensor()
+    
     try:
         while True:
-            print("Lichtstärke : " + str(sensor.readLight()) + " lx")
+                if str(sensor.readLight()):
+                    print("Lichtstärke gut: " + str(sensor.readLight()) + " lx")
+                else:
+                   print("Lichtstärke schlecht: " + str(sensor.readLight()) + " lx")
             time.sleep(1)
     except KeyboardInterrupt:
         pass
 if __name__ == "__main__":
-    main()
+    # cascaded = Anzahl von MAX7219 LED Matrixen, standard=1 
+    # block_orientation = choices 0, 90, -90, standard=0 
+    # rotate = choices 0, 1, 2, 3, Rotate display 0=0°, 1=90°, 2=180°, 3=270°, standard=0 
+    
+    try:
+        main(cascaded=1, block_orientation=90, rotate=0)
+    except KeyboardInterrupt:
+        pass
 
 #Wenn die Lux Anzahl im guten Bereich ist, soll auf der Matrixanzeige ein Symbol erscheinen.
 #Andernfalls ein negatives Symbol
